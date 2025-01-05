45d3117482d0574bd6bebc9dd89a10202b21734a
Abdelrahamn <abdelrahmanahmedkhalifa99@gmail.com>
2025-01-05 13:46:34 +0200

    update json file for production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  apiUrl: 'https://teeefa-001-site1.ntempurl.com/api/',
+  resourceUrl: 'https://teeefa-001-site1.ntempurl.com/',
+};
